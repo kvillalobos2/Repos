1a5b5edbdd54bb4acfc569f65a47162ee71be092
Kvillalobos210 <u201913741@upc.edu.pe>
2021-05-22 01:23:26 -0500

    completo backend

---
+++ StoreApplication/StoreApp.Data/DbContextStoreApp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreApp.Data.Mapping;
+using StoreApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Data
+{
+    public class DbContextStoreApp : DbContext
+    {
+        public DbContextStoreApp(DbContextOptions<DbContextStoreApp> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new ProductMap());
+        }
+    }
+}
